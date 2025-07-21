c9c3fdf9da870955ea9dd96128ccced2f54329eb
Emmanuel Anowai <146497045+emmanuelanowai1@users.noreply.github.com>
2025-07-21 12:12:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 from openrouter import generate_openrouter_advice
-from openrouter import generate_advice_from_openrouter as generate_ai_response
+#from openrouter import generate_advice_from_openrouter as generate_ai_response
 #from gemini import generate_gemini_response
 
 # Load trained model
